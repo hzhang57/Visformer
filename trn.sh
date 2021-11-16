a702369d7188747a611cccabe2cb207f923c26a1
hzhang57 <zhanghaoinf@outlook.com>
2021-11-16 19:24:41 +0800

    fix

--- trn.sh
+++ trn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --model CmtS --batch-size 320 --lr 0.0001 --data-path /ImageNet/ --output_dir /workspace/Visformer/out_cmt
+python -m torch.distributed.launch --nproc_per_node=8 --use_env main_cmt.py --model CmtS --batch-size 320 --lr 0.0001 --data-path /ImageNet/ --output_dir /workspace/Visformer/out_cmt
